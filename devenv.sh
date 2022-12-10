061887c47e7711e74f937f287d19e81cf623ca9f
Ricky Han <rickylqhan@gmail.com>
2022-12-10 21:43:15 +0000

    wip

--- devenv.sh
+++ devenv.sh
@@ -4,6 +4,7 @@ curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 source "$HOME/.cargo/env"
 rustup install stable
 rustup install nightly
+# rustup target add x86_64-unknown-linux-gnu
 rustup component add rust-src --toolchain nightly-x86_64-unknown-linux-gnu
 rustup component add rust-src --toolchain nightly-aarch64-unknown-linux-gnu
 
@@ -24,3 +25,4 @@ npm run build
 popd
 
 cargo xtask build-ebpf --release
+# cargo build --target x86_64-unknown-linux-gnu --release
